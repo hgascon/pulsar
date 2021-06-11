d70527a747228b1bb486a669d752e8ce1c83b9de
kais3r <git@kais3r.de>
2021-06-11 20:37:58 +0200

    2to3: manual adjustments

--- pulsar/core/derrick.py
+++ pulsar/core/derrick.py
@@ -4,11 +4,11 @@ from gzip import GzipFile
 class DerrickPacket:
 
     def __init__(self, line):
-        (self.ntime, self.proto, self.src, self.dst, self.msg) = line.split(" ", 4)
+        (self.ntime, self.proto, self.src, self.dst, self.msg) = line.split(b" ", 4)
         self.ntime = float(self.ntime)
 
     def __str__(self):
-        return " ".join([str(self.ntime), self.proto, self.src, self.dst, self.msg])
+        return " ".join([str(self.ntime), str(self.proto), str(self.src), str(self.dst), str(self.msg)])
 
     def concat(self, newPacket):
         assert(self.src == newPacket.src and
@@ -24,7 +24,7 @@ class DerrickReader:
     def __init__(self, derrickFile):
         self.derrickFile = derrickFile
         g = GzipFile(derrickFile, "rb")
-        self.messages = [DerrickPacket(l.rstrip("\r\n")) for l in g]
+        self.messages = [DerrickPacket(l.rstrip(b"\r\n")) for l in g]
         g.close()
 
 
@@ -36,5 +36,5 @@ class DerrickWriter:
     def writePackets(self, messages):
         g = GzipFile(self.derrickFile, "wb")
         for m in messages:
-            g.write("%s\n" % str(m))
+            g.write(("%s\n" % str(m)).encode())
         g.close()
